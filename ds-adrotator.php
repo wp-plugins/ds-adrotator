db9a296f339e8b11bc866c1a83e638711c7fccc6
Kazantsev <Kazantsev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-10 19:59:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ds-adrotator/trunk@310288 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ds-adrotator.php
+++ ds-adrotator.php
@@ -2,28 +2,60 @@
 /*
 Plugin Name: DS Ad Rotator
 Plugin URI: http://www.ds.ee
-Description: This is a really great plugin that extends WordPress.
-Version: 0.6.2
+Description: This plugin is small banner ad management system for WordPress.
+Version: 0.7
 Author: Jevgeni Kazantsev
 Author URI: http://www.ds.ee
 */
 
-//error_reporting(E_ALL);
 
 if ( is_admin() ) {
-    include_once ABSPATH . 'wp-content/plugins/ds-adrotator/back.php';
+    include_once (WP_PLUGIN_DIR . '/ds-adrotator/back.php');
     $ds_adrotator = new DSAdRotator;
     if ( isset($ds_adrotator) ) {
     	register_activation_hook( __FILE__, array( &$ds_adrotator, 'install' ) );
     	register_deactivation_hook( __FILE__, array( &$ds_adrotator, 'uninstall' ) );
     }
 } else {
-    include_once ABSPATH . 'wp-content/plugins/ds-adrotator/front.php';
-    $ds_adrotator = new DSAdRotator;
+	function ds_show_banners($group_id) {
+		global $wpdb;
+		$ds_adbanners_folder = get_bloginfo('url') . '/wp-content/banners/';
+		$ds_adtables = array(
+				$wpdb->prefix . "dsrotator_banners",
+				$wpdb->prefix . "dsrotator_groups",
+		);
+		$banners_number = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM `".$ds_adtables[0]."` WHERE `group` = '$group_id'"));
+    	$now = current_time('timestamp');
+    	$active_banner = " AND `active` = 'yes' AND '$now' >= `startshow` AND '$now' <= `endshow`";
+    	$group = $wpdb->get_row($wpdb->prepare("SELECT * FROM `" . $ds_adtables[1]."` WHERE `id` = '$group_id'"));
+    	$banners = $wpdb->get_results($wpdb->prepare("SELECT * FROM `" . $ds_adtables[0]."` WHERE `group` = '$group_id'".$active_banner." ORDER BY RAND() LIMIT $group->columns"));
+		foreach ($banners as $banner) {
+				echo '<div class="content-banner">';
+	    		$banner_type = end(explode(".", $banner->banner));
+	    		$banner_file = $ds_adbanners_folder.$banner->banner;
+	    		$swf_link = "&clickTAG=".$banner->link;
+	    		if ( $banner->width != '0' ) {
+	    			$width = $banner->width;
+	    		} else {
+	    			$width = $group->width;
+	    		}
+	    		if ( $banner->height != '0' ) {
+	    			$height = $banner->height;
+	    		} else {
+	    			$height = $group->height;
+	    		}
+	    		if ($banner_type == "swf" || $banner_type == "SWF") {
+	    			if(function_exists('wp_swfobject_echo')) {
+	    				wp_swfobject_echo($banner_file, $width, $height, $swf_link);
+	    			}
+	    		} else { ?>
+	    			<a href="<?php echo $banner->link; ?>"><img style="width : <?php echo $width;?>px; height: <?php echo $height;?>px;" src="<?php echo $banner_file; ?>" /></a>
+	    			<?php
+	    		}
+				echo '</div>';
+	    	}
+	}
 }
-function show_banners($group) {
-	
-	$ds_adrotator->show_banners($group);
-	
-}
+
+
 ?>
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: Kazantsev
 Donate link: http://www.ds.ee
 Tags: advertising, banner
-Requires at least: 3.0 or higher
+Requires at least: 3.0
 Tested up to: 3.0.1.
-Stable tag: 0.6.2
+Stable tag: 0.7
 
 Plugin DS AdRotator allows you to store banners and display them in a given zone.
 
@@ -16,7 +16,7 @@ Plugin DS AdRotator allows you to store banners and display them in a given zone
 
 1. Upload `ds-adrotator.php` to the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Place `<?php $ds_adrotator->show_banners(zoneID); ?>` in your templates
+1. Place `<?php if(function_exists('ds_show_banners')) {ds_show_banners(zoneID);} ?>` in your templates
 
 == Frequently Asked Questions ==
 
@@ -26,12 +26,12 @@ Plugin DS AdRotator allows you to store banners and display them in a given zone
 
 == Changelog ==
 
-= 0.6.2 =
+= 0.7 =
 * Another change.
 
 == Upgrade Notice ==
 
-= 0.6.2 =
+= 0.7 =
 This version contains minor changes. Update immediately.
 
 == Frequently Asked Questions ==
